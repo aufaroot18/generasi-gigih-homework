a91af787f6a3f0dbebee59473b9e1198f79b3ce6
Aufa Billah <aufaroot18@gmail.com>
2021-07-17 14:45:53 +0700

    Add track component in app

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+/* Components */
 import Gif from './components/gif';
-import gif from './data/gif-source';
 import Playlist from './components/playlist';
+import Track from './components/track';
+
+/* Data */
+import gif from './data/gif-source';
 import { data as playlist } from './data/album-source';
+import { data as tracks } from './data/albums-source';
 
 const App = () => {
   return (
     <div className="App">
       <Gif gif={gif} />
       <Playlist playlist={playlist} />
+      <Track tracks={tracks} />
     </div>
   );
 }
