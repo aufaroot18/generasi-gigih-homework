4f0d9941a06ee71735b909a3eba925bcf9bc7d7f
Aufa Billah <aufaroot18@gmail.com>
2021-07-17 14:45:22 +0700

    Create track row header component

---
+++ spotify-clone/src/components/track/TrackRowHeader.js
@@ -0,0 +1,14 @@
+const TrackRowHeader = () => {
+  return(
+    <thead>
+      <tr>
+        <th>Nama Artis</th>
+        <th>Nama Album</th>
+        <th>Tanggal Rilis</th>
+        <th>Total Tracks</th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TrackRowHeader;
