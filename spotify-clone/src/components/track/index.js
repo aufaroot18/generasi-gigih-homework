f3ef0ecfea041a76ae94f9430094f74a42d38447
Aufa Billah <aufaroot18@gmail.com>
2021-07-21 23:19:24 +0700

    Change track component to ff & use hooks

--- spotify-clone/src/components/track/index.js
+++ spotify-clone/src/components/track/index.js
@@ -1,52 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 /* Components */
 import SearchBar from './SearchBar';
 
+import endpoint from '../../globals/spotify-config';
+
 import styles from './Track.module.css';
 
-class Track extends React.Component {
-  constructor(props) {
-    super(props);
-    this.scope = 'playlist-modify-private';
-    this.client_id = 'f5ac16cc1ea44fda9cbe7bb5662252bf';
-    this.redirect_uri = 'http://localhost:3000/';
-    this.link = `https://accounts.spotify.com/id/authorize?response_type=token&client_id=${this.client_id}&scope=${encodeURIComponent(this.scope)}&redirect_uri=${encodeURIComponent(this.redirect_uri)}&show_dialog=true`;
+function Track(props) {
+  const [token, setToken] = useState(null);
 
-    this.state = {
-      token: null,
-    }
-  }
+  useEffect(() => {
+    const params = getParams();
+    setToken(params.get('access_token'));
+  }, []);
 
-  getParams = () => {
+  const getParams = () => {
     const hash = window.location.hash.substr(1);
     const params = new URLSearchParams(hash);
     return params;
   }
 
-  componentDidMount() {
-    const params = this.getParams();
-    const token = params.get('access_token');
-
-    if (token) {
-      this.setState({
-        token
-      })
-    }
-  }
-
-  render() {
-    const { token } = this.state;
-    return(
-      <div className={styles.container}>
-        {
-          this.state.token === null
-          ? <a href={this.link} onClick={this.handleLogin} className={styles.button}>Login Spotify</a>
-          : <SearchBar token={token} />
-        }
-      </div>
-    );
-  }
+  return(
+    <div className={styles.container}>
+      {
+        token === null
+        ? <a href={endpoint} className={styles.button}>Login Spotify</a>
+        : <SearchBar token={token} />
+      }
+    </div>
+  );
 }
 
 export default Track;
