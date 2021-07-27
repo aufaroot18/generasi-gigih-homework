a891f58e0257800d04acfb9f3ac53d07872ac140
Aufa Billah <aufaroot18@gmail.com>
2021-07-27 18:38:08 +0700

    Create gif slice

---
+++ spotify-clone/src/store/gif/gif.slice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// state
+const initialState = {
+  query: '',
+};
+
+// slice
+const gifSlice = createSlice({
+  name: 'gif',
+  initialState,
+  reducers: {
+    setQuery: (state, action) => {
+      state.query = action.payload;
+    }
+  }
+});
+
+// actions
+const { setQuery } = gifSlice.actions;
+
+// reducers
+const gifReducer = gifSlice.reducer;
+
+export{ gifSlice };
+export{ setQuery };
+export default gifReducer;
