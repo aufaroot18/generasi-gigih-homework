29d039c829040f9c899264ed3bae0cb001745bf2
Aufa Billah <aufaroot18@gmail.com>
2021-07-27 18:42:52 +0700

    Dispatch storeToken in track comp

--- spotify-clone/src/components/track/index.js
+++ spotify-clone/src/components/track/index.js
@@ -12,6 +12,10 @@ import styles from './Track.module.css';
 /* Config */
 import endpoint from '../../globals/spotify-config';
 
+
+import { useDispatch } from 'react-redux';
+import { setToken as storeToken } from '../playlist/playlist.slice';
+
 const getParams = () => {
   const hash = window.location.hash.substr(1);
   const params = new URLSearchParams(hash);
@@ -20,9 +24,12 @@ const getParams = () => {
 
 function Track({ token, setToken, uris, setUris, setIsSearched }) {
 
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const params = getParams();
     setToken(params.get('access_token'));
+    dispatch(storeToken(params.get('access_token')));
   }, []);
 
   return(
@@ -30,7 +37,7 @@ function Track({ token, setToken, uris, setUris, setIsSearched }) {
       {
         token === null
         ? <a href={endpoint} className={styles.button}>Login Spotify</a>
-        : <SearchBar token={token} uris={uris} setUris={setUris} setIsSearched={setIsSearched} />
+        : <SearchBar uris={uris} setUris={setUris} setIsSearched={setIsSearched} />
       }
     </div>
   );
