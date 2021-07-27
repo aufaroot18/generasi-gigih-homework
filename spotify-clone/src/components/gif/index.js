446f26f8137c123ce08b3ff8399e87690fdad709
Aufa Billah <aufaroot18@gmail.com>
2021-07-27 18:36:54 +0700

    Use dispatch in gif comp

--- spotify-clone/src/components/gif/index.js
+++ spotify-clone/src/components/gif/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* Package */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+/* Slice */
+import { setQuery } from '../../store/gif/gif.slice';
 
 /* Components */
 import SearchBar from './SearchBar';
@@ -7,24 +11,34 @@ import CardList from './CardList';
 
 /* CSS */
 import styles from './Gif.module.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Gif() {
-  const [search, setSearch] = useState('');
+  // state
+  const [input, setInput] = useState('');
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
+  const { query } = useSelector(state => state.gif);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    _fetchGifs();
+  }, [query])
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
   }
 
   const handleClick = async () => {
+    dispatch(setQuery(input));
     setLoading(true);
     _fetchGifs();
     _addTimeOut(2000);
   }
 
   const _fetchGifs = async () => {
-    const link = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${search}&limit=9`;
+    const link = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${query}&limit=9`;
     const result = await fetch(link);
     const gifs = await result.json();
     setData(gifs.data);
@@ -44,7 +58,7 @@ function Gif() {
 
   return(
     <div className={styles.gif}>
-      <SearchBar handleSearch={handleSearch} handleClick={handleClick} />
+      <SearchBar handleInput={handleInput} handleClick={handleClick} />
       {
         _renderCardList()
       }
