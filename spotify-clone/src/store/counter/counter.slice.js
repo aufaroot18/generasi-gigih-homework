f420beee8465cf0907693085e883fde75ccc8c2e
Aufa Billah <aufaroot18@gmail.com>
2021-07-27 18:31:18 +0700

    Create counter slice

---
+++ spotify-clone/src/store/counter/counter.slice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// initial state
+const initialState = {
+  count: 0,
+}
+
+// slice
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1
+    },
+    decrement: (state) => {
+      state.count -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload
+    },
+  },
+})
+
+// action
+const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// reducer
+const counterReducer = counterSlice.reducer;
+
+export { counterSlice };
+export { increment, decrement, incrementByAmount };
+export default counterReducer;
