00afa7660c4017c2a347b53faa7ccbe41485f16c
Aufa Billah <aufaroot18@gmail.com>
2021-07-27 18:30:01 +0700

    Create global store

---
+++ spotify-clone/src/app/store.js
@@ -0,0 +1,25 @@
+/* 
+
+steps:
+1. Create store
+2. Provide store in index.js
+3. Membuat reducer
+4. Mengimport reducer di store
+5. Menggunaan useSelector untuk mengambil nilai dari store
+5. Gunakan dispatch untuk melakukan aksi
+
+*/
+
+import { configureStore } from '@reduxjs/toolkit';
+import playlistReducer from '../components/playlist/playlist.slice';
+import counterReducer from '../store/counter/counter.slice';
+import gifReducer from '../store/gif/gif.slice';
+
+
+export default configureStore({
+  reducer: {
+    counter: counterReducer,
+    gif: gifReducer,
+    playlist: playlistReducer,
+  }
+});
