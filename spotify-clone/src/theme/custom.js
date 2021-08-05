57b707218e17eeea535f768d6974ad5d4e855402
Aufa Billah <aufaroot18@gmail.com>
2021-08-05 19:41:26 +0700

    Create custom theme for material

---
+++ spotify-clone/src/theme/custom.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@material-ui/core/styles';
+import { teal, indigo } from '@material-ui/core/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: teal,
+    secondary: indigo,
+  },
+});
+
+export default theme;
