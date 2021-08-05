0e5c433700341b16b0cf8559551df06e5588bbe9
Aufa Billah <aufaroot18@gmail.com>
2021-08-05 19:43:44 +0700

    Use Grid Item from material in CardItem

--- spotify-clone/src/components/gif/CardItem.js
+++ spotify-clone/src/components/gif/CardItem.js
@@ -1,21 +1,53 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
 import PropTypes from 'prop-types';
-import styles from './Gif.module.css';
+
+const useStyles = makeStyles({
+  cardImage: {
+    height: 300,
+    objectFit: 'cover',
+  },
+  cardTitle: {
+    marginBottom: '1rem',
+  },
+});
 
 function CardItem({ gif }) {
+  const classes = useStyles();
   return (
-    <div className={styles.image}>
-      <img src={gif.images.original.url} alt="gif" width="100" />
-    </div>
+    <Grid item xs={3}>
+      <Card>
+        <CardMedia
+          className={classes.cardImage}
+          image={gif.images.original.url}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography variant="h5" component="h2" color="primary" gutterBottom>
+            {gif.title}
+          </Typography>
+          <Typography color="textSecondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos modi
+            iste animi dolorem aperiam dolores quisquam consequuntur non, saepe
+            eum.
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 }
 
 CardItem.defaultProps = {
-  gif: PropTypes.string,
+  gif: PropTypes.objectOf(PropTypes.any),
 };
 
 CardItem.propTypes = {
-  gif: PropTypes.string,
+  gif: PropTypes.objectOf(PropTypes.any),
 };
 
 export default CardItem;
