e37a0de22a84c46577a20980ceae539b0f1d1ff6
Aufa Billah <aufaroot18@gmail.com>
2021-08-05 19:41:47 +0700

    Use custom theme in material

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router } from 'react-router-dom';
+import theme from './theme/custom';
 import Navbar from './components/navbar';
 import Routes from './routes';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes />
-    </Router>
+    <>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+          <Routes />
+        </Router>
+      </ThemeProvider>
+    </>
   );
 }
 
