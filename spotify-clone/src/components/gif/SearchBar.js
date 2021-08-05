fb50923254111d6ce635cacee9bfd5bb2c3d6f35
Aufa Billah <aufaroot18@gmail.com>
2021-08-05 19:42:44 +0700

    Use TextField and Button from material

--- spotify-clone/src/components/gif/SearchBar.js
+++ spotify-clone/src/components/gif/SearchBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Gif.module.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+// import styles from './Gif.module.css';
 
 function SearchBar({ handleClick, handleInput }) {
   return (
     <div>
-      <input type="text" onChange={handleInput} className={styles.search} />
-      <button onClick={handleClick} className={styles.button} type="button">Search</button>
+      <Box mb={2}>
+        <TextField id="standard-basic" label="Keyword" onChange={handleInput} />
+      </Box>
+      <Box mb={2}>
+        <Button variant="contained" color="primary" onClick={handleClick}>
+          Search
+        </Button>
+      </Box>
     </div>
   );
 }
