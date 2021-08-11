1263e743a7763adf4a85ad9d9f9621c8ab79aedf
Aufa Billah <aufaroot18@gmail.com>
2021-08-11 19:28:46 +0700

    Remove component notes

--- spotify-clone/src/routes/index.js
+++ spotify-clone/src/routes/index.js
@@ -3,8 +3,6 @@ import { useSelector } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Gif from '../components/gif';
 import Playlist from '../components/playlist';
-import Notes from '../pages/notes';
-import Create from '../pages/notes/create';
 
 function Routes() {
   const { token } = useSelector((state) => state.playlist);
