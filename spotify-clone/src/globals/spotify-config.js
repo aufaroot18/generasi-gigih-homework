6b55b5477a498d114adbf7d50c992cc4f692f81a
Aufa Billah <aufaroot18@gmail.com>
2021-08-14 22:48:59 +0700

    use env in redirect_url

--- spotify-clone/src/globals/spotify-config.js
+++ spotify-clone/src/globals/spotify-config.js
@@ -1,9 +1,8 @@
 const CONFIG = {
   scope: 'playlist-read-private playlist-modify-private',
-  redirect_url: process.env.REACT_APP_REDIRECT_URL,
   client_id: 'f5ac16cc1ea44fda9cbe7bb5662252bf',
 };
 
-const endpoint = `https://accounts.spotify.com/id/authorize?response_type=token&client_id=${CONFIG.client_id}&scope=${encodeURIComponent(CONFIG.scope)}&redirect_uri=${encodeURIComponent(CONFIG.redirect_url)}`;
+const endpoint = `https://accounts.spotify.com/id/authorize?response_type=token&client_id=${CONFIG.client_id}&scope=${encodeURIComponent(CONFIG.scope)}&redirect_uri=${encodeURIComponent(process.env.REACT_APP_REDIRECT_URL)}`;
 
 export default endpoint;
