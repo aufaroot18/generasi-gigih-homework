aa3dc8954a6b7a0419457aa83b979b38e456f2a3
Aufa Billah <aufaroot18@gmail.com>
2021-08-20 08:37:43 +0700

    Remove theme in App Component

--- spotify-clone/src/App.tsx
+++ spotify-clone/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router } from 'react-router-dom';
-import theme from './theme/custom';
 import Navbar from './components/navbar';
 import Routes from './routes';
 import './App.css';
@@ -9,12 +7,10 @@ import './App.css';
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Navbar />
-          <Routes />
-        </Router>
-      </ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes />
+      </Router>
     </>
   );
 }
