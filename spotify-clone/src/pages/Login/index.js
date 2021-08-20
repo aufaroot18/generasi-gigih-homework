aa53628accf6ab5eb2b6fa0a05ecd61e9774ddae
Aufa Billah <aufaroot18@gmail.com>
2021-08-20 08:16:56 +0700

    Create Login Page

--- spotify-clone/src/pages/Login/index.js
+++ spotify-clone/src/pages/Login/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setToken } from '../../store/playlist/playlist.slice';
 import endpoint from '../../globals/spotify-config';
 import styles from './Login.module.css';
+import { getParams } from '../../utils';
 
 function Login() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const params = getParams();
+    if (!!params.get('access_token')) {
+      dispatch(setToken(params.get('access_token')));
+    }
+  }, []);
+
   return (
     <div className={styles.login}>
       <h2 className={styles.login__title}>Enjoy Your Life with Spotify</h2>
